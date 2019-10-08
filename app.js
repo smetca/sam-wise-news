112286a3ec21cfc95ce50aa98e71336742df2d75
smetca <s.metcalfe.h@gmail.com>
2019-10-08 11:06:41 +0100

    Added cors to app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require('./routes/api-router');
 const {handlePsqlErrors, handleCustomErrors} = require('./errors/error-handlers')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', apiRouter);
 
